2973e72c7764b04dfe2c30fb3bcdd5f88596c424
elsuppo <andrew.suponev@gmail.com>
2023-01-02 09:17:36 +0300

    feat: implement reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  if (n >= 0) {
+    return +n.toString().split('').reverse().join('');
+  } else {
+    return +n.toString().slice(1).split('').reverse().join('');
+  }
 }
+
